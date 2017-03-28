a2f9627e71a70b3fbaf3915fe0b9bf62d93a06b0
Alex Arslan <ararslan@comcast.net>
2017-03-27 19:02:32 -0700

    Reformat docstrings and add Documenter docs

---
+++ docs/make.jl
@@ -0,0 +1,19 @@
+using Polynomials, Documenter
+
+makedocs(
+    modules = [Polynomials],
+    clean = false,
+    format = :html,
+    sitename = "Polynomials.jl",
+    authors = "Jameson Nash, Keno Fischer, and other contributors",
+    pages = [
+        "Manual" => "index.md",
+    ],
+)
+
+deploydocs(
+    repo = "github.com/JuliaMath/Polynomials.jl.git",
+    target = "build",
+    deps = nothing,
+    make = nothing,
+)
