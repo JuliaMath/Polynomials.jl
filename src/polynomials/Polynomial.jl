e978eeca6c42d09c6607203531986a56191415c4
Miles Lucas <mdlucas@hawaii.edu>
2019-10-01 17:57:06 -1000

    rewrite roots->fromroots

--- src/polynomials/Polynomial.jl
+++ src/polynomials/Polynomial.jl
@@ -20,14 +20,12 @@ end
 
 domain(p::Polynomial) = (-∞, ∞)
 
-function from_roots(P::Type{Polynomial}, r::AbstractVector{T}, var::SymbolLike = :x) where {T<:Number}
+function _fromroots(P::Type{Polynomial}, r::AbstractVector{T}, var::SymbolLike = :x) where {T <: Number}
     n = length(r)
     c = zeros(T, n + 1)
     c[1] = one(T)
-    for j = 1:n
-        for i = j:-1:1
-            c[i + 1] = c[i + 1] - r[j] * c[i]
-        end
+    for j = 1:n, i = j:-1:1
+        c[i + 1] = c[i + 1] - r[j] * c[i]
     end
     return Polynomial(reverse(c), var)
 end
