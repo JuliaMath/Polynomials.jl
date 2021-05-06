bef45ae1301acc2d68737a2e183f93ba3bd07019
jverzani <jverzani@gmail.com>
2021-05-06 17:21:08 -0400

    dumb unicode...

--- src/polynomials/ngcd.jl
+++ src/polynomials/ngcd.jl
@@ -24,7 +24,7 @@ function ngcd(p::P, q::Q,
     degree(p) < 0  && return (u=q,      v=p, w=one(q),  θ=NaN, κ=NaN)
     degree(p) == 0 && return (u=one(q), v=p, w=q,       θ=NaN, κ=NaN)
     degree(q) < 0  && return (u=one(q), v=p, w=zero(q), θ=NaN, κ=NaN)
-    degree(q) == 0 && return (u=one(p), v=p, w=q,       θ=NaN, κ=NaN)
+    degree(q) == 0 && return (u=one(p), v=p, w=q,       Θ=NaN, κ=NaN)
     p ≈ q          && return (u=p,v=one(p),  w=one(p),  θ=NaN, κ=NaN)
     Polynomials.assert_same_variable(p,q)
 
