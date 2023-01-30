f52aadff21daaeac8d272619f1658fe3708d5c51
singularitti <singularitti@outlook.com>
2023-01-30 00:57:33 -0500

    Add test for `order`

--- test/ChebyshevT.jl
+++ test/ChebyshevT.jl
@@ -10,6 +10,7 @@
         @test p.coeffs == coeff
         @test coeffs(p) == coeff
         @test degree(p) == length(coeff) - 1
+        @test (@test_deprecated order(p)) == length(coeff) - 1
         @test Polynomials.indeterminate(p) == :x
         @test length(p) == length(coeff)
         @test size(p) == size(coeff)
