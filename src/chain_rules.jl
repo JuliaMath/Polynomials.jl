51b1828ad8ebf965d6e5b14f1b8722f3875c65a7
jverzani <jverzani@gmail.com>
2023-03-05 11:08:26 -0500

    correct merge

--- src/chain_rules.jl
+++ src/chain_rules.jl
@@ -2,42 +2,15 @@ import ChainRulesCore
 
 function ChainRulesCore.frule(
     config::ChainRulesCore.RuleConfig{>:ChainRulesCore.HasForwardsMode},
-    (_, ṗ, ẋ),
-    ::AbstractPolynomial,
-    p,
-    x,
-
+    (_, Δx),
+    p::AbstractPolynomial,
+    x
 )
-    @show :hi
-    p(x), derivative(p)(x)
+    p(x), derivative(p)(x)*Δx
 end
 
-# ## modified from
-# ## https://github.com/gdalle/ImplicitDifferentiation.jl/blob/main/src/implicit_function.jl
-# function ChainRulesCore.rrule(
-#     rc::ChainRulesCore.RuleConfig{>:ChainRulesCore.HasReverseMode},
-#     ::typeof(solve),
-#     ZP::ZeroProblem,
-#     M::AbstractUnivariateZeroMethod,
-#     p;
-#     kwargs...,
-# )
-#     xᵅ = solve(ZP, M, p; kwargs...)
 
-#     f(x, p) = first(Callable_Function(M, ZP.F, p)(x))
-#     _, pullback_f = ChainRulesCore.rrule_via_ad(rc, f, xᵅ, p)
-#     _, fx, fp = pullback_f(true)
-#     yp = -fp / fx
-
-#     function pullback_solve_ZeroProblem(dy)
-#         dp = yp * dy
-#         return (
-#             ChainRulesCore.NoTangent(),
-#             ChainRulesCore.NoTangent(),
-#             ChainRulesCore.NoTangent(),
-#             dp,
-#         )
-#     end
-
-#     return xᵅ, pullback_solve_ZeroProblem
-# end
+function ChainRulesCore.rrule(p::AbstractPolynomial, x)
+    _pullback(ΔΩ) = (ChainRulesCore.NoTangent(), derivative(p)(x))
+    return (p(x), _pullback)
+end
