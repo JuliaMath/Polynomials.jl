c1c01c9c09e0fa64b09589106099c2bc9288e6c8
KristofferC <kristoffer.carlsson@juliacomputing.com>
2023-03-13 13:54:21 +0100

    move MakieCore and ChainRulesCore code to extensions

--- src/makie.jl
+++
@@ -1,8 +0,0 @@
-import MakieCore
-
-function MakieCore.convert_arguments(P::Type{<:MakieCore.XYBased}, p::AbstractPolynomial) 
-    xs = poly_interval(p)
-    return MakieCore.convert_arguments(P, xs, p.(xs))
-end
-
-MakieCore.plottype(p::AbstractPolynomial) = MakieCore.Lines
