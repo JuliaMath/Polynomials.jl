a90cc5ec1824b30aeb4b9612b43f628e738e65ce
jverzani <jverzani@gmail.com>
2023-04-25 19:58:07 -0400

    use _convert

--- src/common.jl
+++ src/common.jl
@@ -1048,12 +1048,10 @@ end
 scalar_mult(p1::AbstractPolynomial, p2::AbstractPolynomial) = error("scalar_mult(::$(typeof(p1)), ::$(typeof(p2))) is not defined.") # avoid ambiguity, issue #435
 
 # scalar div
-Base.:/(p::P, c::S) where {P <: AbstractPolynomial,S} = scalar_div(p, c)
+Base.:/(p::AbstractPolynomial, c) = scalar_div(p, c)
 function scalar_div(p::P, c::S) where {S, T, X, P<:AbstractPolynomial{T, X}}
     iszero(p) && return zero(⟒(P){Base.promote_op(/,T,S), X})
-
-    result = coeffs(p) ./ (c,)
-    ⟒(P){eltype(result), X}(result)
+    _convert(p, coeffs(p) ./ Ref(c))
 end
 
 ## Polynomial p*q
